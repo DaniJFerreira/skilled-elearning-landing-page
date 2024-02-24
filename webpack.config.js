3e27da3c5d7ba5a22ef474902f45eea683711b7f
DaniJFerreira <daniel.jorge.ferreira1984@gmail.com>
2024-02-24 12:24:47 +0000

    implemented webpack config and add sass,babel and webpack npm  dependancies

---
+++ webpack.config.js
@@ -0,0 +1,199 @@
+// const path = require("path");
+// const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+// const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+// const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+// const TerserPlugin = require("terser-webpack-plugin");
+// const CopyWebpackPlugin = require("copy-webpack-plugin");
+// const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+// let mode = "development";
+
+// if (process.env.NODE_ENV === "production") {
+//   mode = "production";
+// }
+
+// module.exports = {
+//     entry:{
+//         main:"./src/main.js"
+//     },
+//     mode: mode,
+//     output:{
+//         path: path.resolve(__dirname, 'dist'),
+//         filename: '[name].[contenthash].bumdles.js',
+//     },
+//     optimization:{
+//         minimize: true,
+//         minimizer: [
+//             new TerserPlugin(),
+//             new CssMinimizerPlugin()
+//         ],
+//     minimize: true,
+//     },
+//     plugins:[
+//         new MiniCssExtractPlugin({
+//             filename: "[name].[contenthash].css",
+//         }),
+//         new CleanWebpackPlugin(),
+//         new CopyWebpackPlugin({
+//             patterns: [
+//                 { from: "public", to: " " },
+//             ],
+//         }),
+//         new HtmlWebpackPlugin({
+//             template: "./src/template.html",
+//             ninify: {
+//                 removeComments: true,
+//                 collapseWhitespace: true,
+//                 removeComments: true,
+//             }
+//         }),
+//     ],
+//     module:{
+//         rules:[
+//             {
+//                 test: /\.html$/,
+//                 use: [
+//                     "html-loader",
+//                 ]
+//             },
+//             {
+//                 test:/\.(png|jpe?g|gif|svg)$/i,
+//                 type: "asset/resource",
+//                 generator:{
+//                     filename: "assets/[name][ext]"
+//                 }
+//             },
+//             {
+//                 test: /\.s?[ac]css$/,
+//                 use: [
+//                    {
+//                     loader: MiniCssExtractPlugin.loader
+//                    },
+//                     "css-loader",
+//                     "sass-loader"
+//                 ],
+//             },
+//             {
+//                 test: /\.js$/,
+//                 exclude: /node_modules/,
+//                 use: {
+//                     loader: "babel-loader",
+//                     options: {
+//                         presets: ["@babel/preset-env"],
+//                     },
+//                 },
+//             },
+//         ]
+//     },
+//     devServer: {
+//         static: {
+//             directory: path.join(__dirname, "dist"),
+//         },
+//         compress: true,
+//         port: 3000,
+//     },
+//     devtool: mode === "development"?'eval-source-map' : 'source-map'
+// };
+
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+
+let mode = "development";
+
+if (process.env.NODE_ENV === "production") {
+  mode = "production";
+}
+
+module.exports = {
+  entry: {
+    main:"./src/main.js",
+  },
+
+  mode: mode,
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[contenthash].bundle.js",
+  },
+  optimization: {
+    minimizer: [
+      new OptimizeCssAssetsPlugin(),
+      new TerserPlugin(),
+    ],
+    minimize: true,
+  },
+  plugins: [
+    new MiniCssExtractPlugin({ filename: "[name].[contenthash].bundle.css"}),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+      minify: {
+        removeAttributeQuotes: true,
+        collapseWhitespace: true,
+        removeComments: true
+      } 
+   }),
+   new CopyWebpackPlugin({
+    patterns: [{ from: 'public/', to: '' }],
+  }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: [
+          "html-loader"
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: 'assets/[name][ext]', 
+        },
+      },
+      {
+        test: /\.s?[ac]ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          "css-loader", 
+          "sass-loader",
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            cacheDirectory: true,
+          }
+        },
+      },
+      {
+        test: /\.json$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'data/[name][ext]', 
+        },
+      },
+    ],
+  },
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 3000,
+  },
+  
+  devtool: mode === "development" ? 'eval-source-map' : 'source-map',
+};
